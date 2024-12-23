06ffb520ffada8e1fafb28f3e540c7c41502a8b1
Dat Nguyen <nguyendat5497@gmail.com>
2024-12-23 07:47:09 +0700

    new

---
+++ PageObjectModel/src/Test/Base/BaseSetup.java
@@ -0,0 +1,5 @@
+package Test.Base;
+
+public class BaseSetup {
+
+}
---
+++ PageObjectModel/src/Test/Base/ValidateHelper.java
@@ -0,0 +1,11 @@
+package Test.Base;
+
+import java.sql.Driver;
+
+public class ValidateHelper {
+	@Test
+	public void Test1() {
+		
+	}
+
+}
